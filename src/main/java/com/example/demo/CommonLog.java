bd7479a818674933c852da46f4304d5747b85fb2
riya-reji-2021 <96300121+riya-reji-2021@users.noreply.github.com>
2022-01-17 22:13:39 +0530

    Update CommonLog.java

--- src/main/java/com/example/demo/CommonLog.java
+++ src/main/java/com/example/demo/CommonLog.java
@@ -11,8 +11,8 @@ import org.apache.beam.sdk.schemas.annotations.DefaultSchema;
 
 @DefaultSchema(JavaFieldSchema.class)
 public class CommonLog {
-    Integer id;
-    String name;
-    String surname;
+    @javax.annotation.Nullable int id;
+    @javax.annotation.Nullable String name;
+    @javax.annotation.Nullable String surname;
 }
 
